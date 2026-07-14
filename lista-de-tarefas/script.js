dbc3eab2ad0acaec2d7f46b2be4a020d72db95ca
suzana09 <suzanabercacollo@gmail.com>
2026-07-14 13:48:55 -0300

    Update script.js

---
+++ lista-de-tarefas/script.js
@@ -0,0 +1,49 @@
+body {
+
+    text-align: center;
+    font-family: Arial, sans-serif;
+
+}
+
+
+input {
+
+    padding: 10px;
+
+    font-size: 16px;
+
+}
+
+
+button {
+
+    padding: 10px 20px;
+
+    cursor: pointer;
+
+}
+
+
+#aviso {
+
+    color: red;
+
+}
+
+
+.estudar {
+
+    font-weight: bold;
+
+}
+
+
+li {
+
+    list-style-position: inside;
+
+    font-size: 20px;
+
+    margin: 10px;
+
+}
